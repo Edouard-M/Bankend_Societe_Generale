1d9c07189651340a7a6bbfbf3ef423d25dad2b7d
Edouard-M <edou17@laposte.net>
2023-03-18 18:19:52 +0100

    Get balance

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const port = 3001
 
 app.use(express.json())
 
+app.get('/balance', (req, res) => {
+    result = bank.get_balance();
+    res.status(200).json(result)
+})
+
 //default route
 app.use('*', (req, res) => {
     res.status(404).json('not found')
--- tests/bank.test.js
+++ tests/bank.test.js
@@ -1,1 +1,12 @@
 const bank = require('../lib/bank.js')
+
+beforeEach(() => {
+    bank.clear()
+});
+
+describe('balance', () => {
+
+    test('verify balance amount is 0', () => {
+        expect(bank.get_balance()).toBe(0);
+    });
+});
